3945f5064d24d5490a36e34a08e3c4f63584795a
Hung Vu <hungvu@Hungs-MacBook-Pro.local>
2023-11-08 20:11:23 +0700

    init project

---
+++ GitDemo/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  GitDemo
+//
+//  Created by Hung Vu on 08/11/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
